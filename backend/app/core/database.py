1745c0f35070acbaa83671ab0bf8f76f94b690bb
FedericoSanin <federico16_12@hotmail.com>
2025-10-19 16:00:21 -0500

    Changes database, gitignore

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -7,10 +7,26 @@ DATABASE_URL = (
     f"@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
 )
 
-print("DATABASE_URL:", DATABASE_URL)
+#print("DATABASE_URL:", DATABASE_URL)
 
 #Crear el motor de conexión
 engine = create_engine(DATABASE_URL)
 
 #Crear una sesión (interfaz etre FastAPI y la base de datos)
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
+
+# Función que genera una sesión de BD en cada request
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+if __name__ == "__main__":
+    print("🔍 Probando conexión a la base de datos...")
+    try:
+        with engine.connect() as connection:
+            print("✅ Conexión exitosa a la base de datos")
+    except Exception as e:
+        print("❌ Error de conexión:", e)
